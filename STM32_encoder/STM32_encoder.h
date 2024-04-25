f80cde22022d9a73a892758cc089a49f62b7fb35
kkmecha <167722055+kkmecha@users.noreply.github.com>
2024-04-25 22:46:32 +0900

    Update STM32_encoder.h

--- STM32_encoder/STM32_encoder.h
+++ STM32_encoder/STM32_encoder.h
@@ -1,34 +1,33 @@
 #ifndef __STM32_ENCODER_H__
 #define __STM32_ENCODER_H__
 
-#include "mbed.h"
-#include <stdint.h>
+// #include "mbed.h"
+// #include "stm32f7xx.h"
+// #include "stm32f767xx.h"
+// #include "stm32f7xx_hal_tim_ex.h"
+// #include "system_stm32f7xx.h"
 
-#define TIM3_TIM4_PERIOD 0xffff
-#define MAX_TIMER_VALUE_HALF 32768
+#define MAX_TIMER_VALUE_HALF 32768 //16bitの最高値の半分だから32768
 
 class STM32_encoder{
-    public:
-        STM32_encoder(PinName a, PinName b);
-        void start();
-        void reset();
-        int32_t get_count();
+    public: 
+    STM32_encoder(PinName a, PinName b);
+    void start();
+    void reset();
+    int32_t get_count();
 
     private:
-        enum class _timer_select{
-            tim_3,
-            tim_4
-        };
-        TIM_HandleTypeDef _timer;
-        TIM_Encoder_InitTypeDef _encoder;
-        GPIO_InitTypeDef _GPIO_InitStruct;
-        _timer_select _tim;
-        void _HAL_TIM_Encoder_MspInit(TIM_HandleTypeDef *htim, PinName a, PinName b);
-        void _timer_setting();
-        void _encoder_setting();
+    TIM_HandleTypeDef _htim;
+    TIM_Encoder_InitTypeDef encoder;
 
-        static int32_t _encoder_high_bits;
-        int32_t _hbits = 0, _count = 0;
+    void HAL_TIM_Encoder_MspInit(TIM_HandleTypeDef *htim, PinName a, PinName b);
+    void _timer_instance_setting();
+    void _encoder_instance_setting();
+    enum class _timer_select{
+        tim_3,
+        tim_4
+    };
+    _timer_select _tim;
 };
 
 #endif
