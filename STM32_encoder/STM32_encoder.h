dd5dc1371742800e587fc420843217480244e10f
kkmecha <167722055+kkmecha@users.noreply.github.com>
2024-05-02 08:53:20 +0900

    STM32_encoder.h を更新

--- STM32_encoder/STM32_encoder.h
+++ STM32_encoder/STM32_encoder.h
@@ -22,14 +22,14 @@ extern const TIM_Pin_Map tim_mappings[];
 class STM32_encoder{
     public:
     STM32_encoder(PinName slit_a, PinName slit_b, int resolution, int times);
-    void GPIO_InitPeriph(PinName slit_a, PinName slit_b);
-    void start();
-    void reset();
-    int32_t get_count();
+    void encoder_start();
+    void encoder_reset();
+    int32_t encoder_get_count();
 
     private:
     PinName _a, _b;
     int _resolution = 200, _times = 4; // 分解能, 何逓倍か
+    void 
     TIM_HandleTypeDef _htim;
     GPIO_InitTypeDef _GPIO_InitStruct;
     TIM_Encoder_InitTypeDef _encoder;
