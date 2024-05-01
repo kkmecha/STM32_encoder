5749a76b5d0385b264dc08810ab8dab9b5b0f4f3
kkmecha <167722055+kkmecha@users.noreply.github.com>
2024-05-02 08:58:39 +0900

    SampleCode.cpp を更新

--- SampleCode.cpp
+++ SampleCode.cpp
@@ -1,6 +1,7 @@
 #include "mbed.h"
 #include "STM32_encoder.h"
 
+// 第3、第4引数(分解能と逓倍の指定)は省略できます
 STM32_encoder encoder1(PC_6, PC_7, 800, 4); // 分解能800
 STM32_encoder encoder2(PD_12, PD_13, 200, 4); // 分解能200
 
