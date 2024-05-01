393f831e2a5a402d053ad46ede8a1a1497b1f05e
kkmecha <167722055+kkmecha@users.noreply.github.com>
2024-05-01 22:57:22 +0900

    Update SampleCode.cpp

--- SampleCode.cpp
+++ SampleCode.cpp
@@ -1,17 +1,18 @@
 #include "mbed.h"
 #include "STM32_encoder.h"
 
-STM32_encoder encoder1(PC_6, PC_7);
-STM32_encoder encoder2(PD_12, PD_13);
+STM32_encoder encoder1(PC_6, PC_7, 800, 4); // 分解能800
+STM32_encoder encoder2(PD_12, PD_13, 200, 4); // 分解能200
 
-int main()
-{
+int32_t a, b;
+
+int main(){
     encoder1.start();
     encoder2.start();
     encoder1.reset();
     encoder2.reset();
     while(true){
-        printf("encoder1:%ldencoder2:%ld\r\n", encoder1.get_count(), encoder2.get_count());
+        printf("angle1:%ld, angle2:%ld\r\n", encoder1.get_count(), encoder2.get_count());
         ThisThread::sleep_for(5ms);
     }
 }
