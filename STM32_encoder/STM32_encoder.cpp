4d8fd1087bb9612beaa61dd9614ada112c401fd3
kkmecha <167722055+kkmecha@users.noreply.github.com>
2024-05-02 12:37:52 +0900

    Update STM32_encoder.cpp

--- STM32_encoder/STM32_encoder.cpp
+++ STM32_encoder/STM32_encoder.cpp
@@ -1,4 +1,5 @@
-//         _bhitsの動き         ...      1     -> 0 ->    1   ...
+//                                  2^32 - 1
+//         _bhitsの動き         ...4294967295 -> 0 ->    1   ...
 //   (_hbits << 16) | _count    ...-65536~-1  -> 0 -> 1~65536...
 #include "mbed.h"
 #include "STM32_encoder.h"
@@ -9,27 +10,74 @@ const TIM_Pin_Map tim_mappings[] = { // 独自のpinmap
     // TIM_TypeDef*, GPIO_TypeDef*, (PinName, uint16_t)x2, uint8_t
     #ifdef TARGET_STM32F072RB
     #elif defined (TARGET_STM32F303K8)
+    // TIM2...32bit TIM3...16bit
+    // Max Interface clock..72, Man timer clock...72
+    //16bit
+    
+
+
     #elif defined (TARGET_STM32F401RE)
+    // TIM2, TIM5...32bit TIM3, TIM4...16bit
+    // Max Interface clock..42, Man timer clock...84
+    // 16bit
+    {TIM3, GPIOA, PA_6,  GPIO_PIN_6,  PA_7,  GPIO_PIN_7,  GPIO_AF2_TIM3},
+    {TIM3, GPIOB, PB_4,  GPIO_PIN_4,  PB_5,  GPIO_PIN_5,  GPIO_AF2_TIM3},
+    {TIM3, GPIOC, PC_6,  GPIO_PIN_6,  PC_7,  GPIO_PIN_7,  GPIO_AF2_TIM3},
+    {TIM4, GPIOB, PB_6,  GPIO_PIN_6,  PB_7,  GPIO_PIN_7,  GPIO_AF2_TIM4},
+    {TIM4, GPIOD, PD_12, GPIO_PIN_12, PD_13, GPIO_PIN_13, GPIO_AF2_TIM4},
+
+    // 32bit
+    {TIM2, GPIOA, PA_0, GPIO_PIN_0, PA_1, GPIO_PIN_1, GPIO_AF1_TIM2},
+    {TIM2, GPIOA, PA_5, GPIO_PIN_5, PA_1, GPIO_PIN_1, GPIO_AF1_TIM2},
+    {TIM2, GPIOA, PA_15, GPIO_PIN_15, PA_1, GPIO_PIN_1, GPIO_AF1_TIM2},
+    {TIM5, GPIOA, PA_0, GPIO_PIN_0, PA_1, GPIO_PIN_1, GPIO_AF2_TIM5},
+    // {TIM5, GPIOH, PH_10, GPIO_PIN_10, PH_11, GPIO_PIN_11, GPIO_AF2_TIM5}, データシート上にはあるがPH_10などはない
+
+
     #elif defined (TARGET_STM32F746ZG)
+    // TIM2, TIM5...32bit TIM3, TIM4...16bit
+    // Max Interface clock..54, Man timer clock...216
+    // 16bit
+    {TIM3, GPIOA, PA_6,  GPIO_PIN_6,  PA_7,  GPIO_PIN_7,  GPIO_AF2_TIM3},
+    {TIM3, GPIOB, PB_4,  GPIO_PIN_4,  PB_5,  GPIO_PIN_5,  GPIO_AF2_TIM3},
+    {TIM3, GPIOC, PC_6,  GPIO_PIN_6,  PC_7,  GPIO_PIN_7,  GPIO_AF2_TIM3},
+    {TIM4, GPIOB, PB_6,  GPIO_PIN_6,  PB_7,  GPIO_PIN_7,  GPIO_AF2_TIM4},
+    {TIM4, GPIOD, PD_12, GPIO_PIN_12, PD_13, GPIO_PIN_13, GPIO_AF2_TIM4},
+
+    // 32bit
+    {TIM2, GPIOA, PA_0, GPIO_PIN_0, PA_1, GPIO_PIN_1, GPIO_AF1_TIM2},
+    {TIM2, GPIOA, PA_5, GPIO_PIN_5, PA_1, GPIO_PIN_1, GPIO_AF1_TIM2},
+    {TIM2, GPIOA, PA_15, GPIO_PIN_15, PA_1, GPIO_PIN_1, GPIO_AF1_TIM2},
+    {TIM5, GPIOA, PA_0, GPIO_PIN_0, PA_1, GPIO_PIN_1, GPIO_AF2_TIM5},
+    // {TIM5, GPIOH, PH_10, GPIO_PIN_10, PH_11, GPIO_PIN_11, GPIO_AF2_TIM5}, データシート上にはあるがPH_10などはない
     #elif defined (TARGET_STM32F767ZI)
+    // TIM2, TIM5...32bit TIM3, TIM4...16bit
+    // Max Interface clock..54, Man timer clock...216
+    // 16bit
     {TIM3, GPIOA, PA_6,  GPIO_PIN_6,  PA_7,  GPIO_PIN_7,  GPIO_AF2_TIM3},
     {TIM3, GPIOB, PB_0,  GPIO_PIN_0,  PB_1,  GPIO_PIN_1,  GPIO_AF2_TIM3},
     {TIM3, GPIOB, PB_4,  GPIO_PIN_4,  PB_5,  GPIO_PIN_5,  GPIO_AF2_TIM3},
     {TIM3, GPIOC, PC_6,  GPIO_PIN_6,  PC_7,  GPIO_PIN_7,  GPIO_AF2_TIM3},
     {TIM4, GPIOB, PB_6,  GPIO_PIN_6,  PB_7,  GPIO_PIN_7,  GPIO_AF2_TIM4},
     {TIM4, GPIOD, PD_12, GPIO_PIN_12, PD_13, GPIO_PIN_13, GPIO_AF2_TIM4},
+
+    // 32bit
+    {TIM2, GPIOA, PA_0, GPIO_PIN_0, PA_1, GPIO_PIN_1, GPIO_AF1_TIM2},
+    {TIM2, GPIOA, PA_5, GPIO_PIN_5, PA_1, GPIO_PIN_1, GPIO_AF1_TIM2},
+    {TIM2, GPIOA, PA_15, GPIO_PIN_15, PA_1, GPIO_PIN_1, GPIO_AF1_TIM2},
+    {TIM5, GPIOA, PA_0, GPIO_PIN_0, PA_1, GPIO_PIN_1, GPIO_AF2_TIM5},
+    // {TIM5, GPIOH, PH_10, GPIO_PIN_10, PH_11, GPIO_PIN_11, GPIO_AF2_TIM5}, データシート上にはあるがPH_10などはない
     #elif
     #error "STM32_encoder's pinmap not found\r\n"
     #endif
 };
 
-STM32_encoder::STM32_encoder(PinName slit_a, PinName slit_b, int resolution = 200, int times = 4)
- : _a(slit_a), _b(slit_b), _resolution(resolution), _times(times)
+STM32_encoder::STM32_encoder(PinName slit_a, PinName slit_b, int resolution = 200, int times = 4) : _a(slit_a), _b(slit_b), _resolution(resolution), _times(times)
 {
-    _GPIO_InitPeriph(slit_a, slit_b);
+    GPIO_InitPeriph(slit_a, slit_b);
 }
  
-void STM32_encoder::_GPIO_InitPeriph(PinName slit_a, PinName slit_b)
+void STM32_encoder::GPIO_InitPeriph(PinName slit_a, PinName slit_b)
 {
     for(const TIM_Pin_Map& mapping : tim_mappings){ // 配列の全要素の走査
         if(mapping.Pin_name.pin_a == slit_a && mapping.Pin_name.pin_b == slit_b){ // 引数で指定されたピンがTIM_Pin_Mapとあっているか確認
@@ -60,7 +108,7 @@ void STM32_encoder::_GPIO_InitPeriph(PinName slit_a, PinName slit_b)
     }
 }
 
-void STM32_encoder::encoder_start(){
+void STM32_encoder::start(){
     // timer
     for(const TIM_Pin_Map& mapping : tim_mappings){
         if(mapping.Pin_name.pin_a == _a && mapping.Pin_name.pin_b == _b){
@@ -88,26 +136,24 @@ void STM32_encoder::encoder_start(){
     HAL_TIM_Encoder_Start(&_htim,TIM_CHANNEL_ALL);
 }
 
-void STM32_encoder::encoder_reset(){
+void STM32_encoder::reset(){
     core_util_critical_section_enter();
     __HAL_TIM_CLEAR_FLAG(&_htim, TIM_IT_UPDATE);
     core_util_critical_section_exit();
 }
 
-int32_t  STM32_encoder::encoder_get_count(){
-    int32_t _count, _hbits, _angle;
-    static int _encoder_high_bits = 0;
+int32_t  STM32_encoder::get_count(){ // 16bitの時の処理のみしか書いてない クロック数の違いにも対応したい
+    int32_t _count, _angle;
     core_util_critical_section_enter();
     _count = _htim.Instance->CNT;
     if((_htim.Instance->SR & (TIM_FLAG_UPDATE)) == TIM_FLAG_UPDATE){
         _htim.Instance->SR = ~(TIM_IT_UPDATE);
         if(_htim.Instance->CNT < 32768)
-            _encoder_high_bits += 1;
+            _hbits += 1;
         else
-            _encoder_high_bits -= 1;
+            _hbits -= 1;
         _count = _htim.Instance->CNT;
     }
-    _hbits = _encoder_high_bits;
     _angle = int32_t(((_hbits << 16) | _count) / float(_resolution * _times / 360.0)); // 発生したパルス数 / 1°あたりのパルス数  (発生したパルス数を度数法に変換)
     core_util_critical_section_exit();
     //printf("count:%lu, hbits:%lu, (_hbits << 16) | _count:%ld\r\n", _count, _hbits, (_hbits << 16) | _count);
